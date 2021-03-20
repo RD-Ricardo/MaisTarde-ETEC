9a5d63e4b20baaed31b45b100f1336c654c8616a
RD-Ricardo <ricardodias169@gmail.com>
2021-03-20 18:06:21 -0300

    Finalizando Programa

---
+++ Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+
+namespace MaisTarde
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {   
+            //Data de Agora
+            DateTime now = DateTime.Now;
+            string HoursNow = now.ToShortTimeString();
+            //Data depos de 6 horas e 30 minutos
+            DateTime later = now.AddHours(6.30);
+            string laterTime = later.ToShortTimeString();
+            Console.WriteLine("--------------");
+            Console.WriteLine("--Mais tarde--");
+            Console.WriteLine("--------------");
+            Console.Write($"Agora {HoursNow}, mais tarde {laterTime}.");
+        }
+    }
+}
